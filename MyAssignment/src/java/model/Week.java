b025a2c587287685ceb8fb99fcb566200239cc40
Nguyen Nam Khanh <nnamkhanh002@gmail.com>
2022-07-14 20:15:29 +0700

    add week entity

---
+++ MyAssignment/src/java/model/Week.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author KakaNoob
+ */
+public class Week {
+    private Date startDate;
+    private Date endDate;
+
+    public Week() {
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    
+    
+}
--- MyAssignment/web/view/LoginError.jsp
+++ MyAssignment/web/view/LoginError.jsp
@@ -10,9 +10,23 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
+            <script>
+            var count = 3;
+            function redirect()
+            {
+                count--;
+                document.getElementById('timer').innerHTML = count;
+                if(count <= 0)
+                {
+                    window.location.href = '../login';
+                }
+            }
+            setInterval(redirect,1000);
+        </script>
     </head>
     <body>
         <h1>FPT University Academic Portal</h1>
         <h2>Login Failed!</h2>
+     Redirect to login page after <span id="timer">3</span> seconds!
     </body>
 </html>
