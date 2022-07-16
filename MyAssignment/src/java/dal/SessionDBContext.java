c9a6c3d6ea0a183fb554f79a887fff40845a14b1
Nguyen Nam Khanh <nnamkhanh002@gmail.com>
2022-07-17 02:43:51 +0700

    get sessions by session id

--- MyAssignment/src/java/dal/SessionDBContext.java
+++ MyAssignment/src/java/dal/SessionDBContext.java
@@ -69,4 +69,55 @@ public class SessionDBContext extends DBContext<Session> {
         }
         return selist;
     }
+    public ArrayList<Session> listSessionById(int seid){
+        ArrayList<Session> selist = new ArrayList<>();
+        try {
+            String sql = "select s.sessionID , s.sessionDate , sg.stuGroup,\n" +
+"                     g.gName , l.[password], l.lectureName ,l.[login] , l.email"
+                    + ", su.subID , su.subName , s.RoomID , s.SlotNO \n" +
+"                   from [Session] s join Room r on s.RoomID = r.Room_ID\n" +
+"                    inner join Student_Group sg on sg.stuGroup = s.stuGroup\n" +
+"                    inner join Slot sl on sl.slotNO = s.SlotNO\n" +
+"                    inner join Lecturer l on sg.lecture_Login = l.[login]\n" +
+"                    inner join [Group] g on sg.gname = g.gname\n" +
+"                    inner join [Subject] su on sg.subID = su.subID\n" +
+"                    where s.sessionID = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, seid);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next()){
+                Session s = new Session();
+                s.setDate(rs.getDate("sessionDate"));
+                s.setId(rs.getInt("sessionID"));
+                Room r = new Room();
+                r.setName(rs.getString("RoomID"));
+                s.setRoom(r);
+                Slot sl = new Slot(rs.getInt("SlotNo"));
+                s.setSlot(sl);
+                //StudentGroup
+                StudentGroup sg = new StudentGroup();
+                Group g = new Group();
+                g.setName(rs.getString("gName"));
+                sg.setGroup(g);
+                sg.setId(rs.getString("stuGroup"));
+                //Lecturer
+                Lecturer l = new Lecturer();
+                l.setEmail(rs.getString(8));
+                l.setLogin(rs.getString(7));
+                l.setName(rs.getString(6));
+                l.setPassword(rs.getString(5));
+                sg.setLecturer(l);
+                Subject su = new Subject();
+                su.setId(rs.getString("subID"));
+                su.setName(rs.getString("subName"));
+                sg.setSubject(su);
+                s.setStuGroup(sg);
+                //Add to sessionlist
+                selist.add(s);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return selist;
+    }
 }
