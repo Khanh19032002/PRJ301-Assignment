e2a4c96672929d5873cf1e2c68bf873c65a47b34
Nguyen Nam Khanh <nnamkhanh002@gmail.com>
2022-08-18 10:41:27 +0700

    Update Assignment.sql

--- Assignment.sql
+++ Assignment.sql
@@ -8,21 +8,21 @@ select s.sessionID , s.sessionDate , sg.stuGroup,
                     inner join [Group] g on sg.gname = g.gname
                     inner join [Subject] su on sg.subID = su.subID
                     where l.login = ?
+
 select * from Slot
+
 --Attendance SQL
 select a.aid , s.[sID] , s.[login] , s.sName
 , se.sessionID , se.stuGroup ,a.[status] from Attend a 
  inner join [Session] se on a.sessionID = se.sessionID
  inner join Student s on s.[sID] = a.[stuID]
- inner join Enroll e on s.[sID] = e.[sID]
- inner join Student_Group sg on e.stuGroup = sg.stuGroup
- where se.sessionID = 8
+ where se.sessionID = ?
 
 select s.[login] , s.[sID] , s.[sName] from [Session] se
 inner join Student_Group sg on se.stuGroup = sg.stuGroup 
 inner join Enroll e on e.stuGroup = sg.stuGroup
 inner join Student s on s.[sID] = e.[sID]
-where se.sessionID = 1 
+where se.sessionID = ?
 
 select s.sessionID , s.sessionDate , sg.stuGroup,
                      g.gName , l.[password], l.lectureName ,l.[login] , l.email, su.subID , su.subName , s.RoomID , s.SlotNO 
@@ -32,7 +32,7 @@ select s.sessionID , s.sessionDate , sg.stuGroup,
                     inner join Lecturer l on sg.lecture_Login = l.[login]
                     inner join [Group] g on sg.gname = g.gname
                     inner join [Subject] su on sg.subID = su.subID
-                    where s.sessionID = 1
+                    where s.sessionID = ?
 
 --Insert and Update Attendance
 INSERT INTO [dbo].[Attend]
@@ -42,10 +42,7 @@ INSERT INTO [dbo].[Attend]
      VALUES
            (?,?,?)
 
-Update [Attend] set [status]= '0' where aid = '4'
-
-use [Assignment]
-select * from attend
+Update [Attend] set [status]= ? where aid = ?
 
 
 --Report sql
